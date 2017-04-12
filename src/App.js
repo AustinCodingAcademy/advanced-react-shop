b0ecc44fd165cc534d3c4441535e524bc677d5f0
Jon Woo <jw56578@gmail.com>
2017-04-12 13:28:38 -0500

    filter on category

--- src/App.js
+++ src/App.js
@@ -5,35 +5,50 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import ProductDetail from './components/ProductDetail';
 import Carousel from './components/Carousel';
-function App (props) {
 
-    var products = props.state.products.map(function(prod){
-        return <ProductDetail product={prod} />;
-    });
-    return (
-          <div className="App">
-            <NavBar />
-            <div className="container">
-              <div className="row">
-                  <div className="col-md-3">
-                      <p className="lead">Shop Name</p>
-                      <div className="list-group">
-                          <a href="#" className="list-group-item">Category 1</a>
-                          <a href="#" className="list-group-item">Category 2</a>
-                          <a href="#" className="list-group-item">Category 3</a>
-                      </div>
-                  </div>
-                  <div className="col-md-9">
-                      <Carousel />
-                      <div className="row">
-                        {products}
-                      </div>
-                  </div>
-              </div>
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {category:"food"}
+    }
+    render(){
+        var products = this.props.state.products.filter((prod)=>{
+            return prod.category === this.state.category;
+        });
+        products = products.map(function(prod){
+            return <ProductDetail product={prod} />;
+        });
+        var categories = this.props.state.categories.map((cat)=>{
+            return  <a onClick={
+                ()=>{
+                    this.setState({category:cat});
+                }
+            } 
+            href="#" className="list-group-item">{cat}</a>;
+        });
+        return (
+            <div className="App">
+                <NavBar />
+                <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <p className="lead">Shop Name</p>
+                        <div className="list-group">
+                            {categories}
+                        </div>
+                    </div>
+                    <div className="col-md-9">
+                        <Carousel />
+                        <div className="row">
+                            {products}
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-          </div>
-    );
+        );
+    }
 }
 
 export default App;
